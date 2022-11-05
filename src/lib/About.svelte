0a00aa020ebf313f7e1ba2eb36a47fb7fcbe41fb
Rodrigo <gokusan@keemail.me>
2022-11-05 07:59:21 -0600

    :zap:

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -1,7 +1,7 @@
 <section class="about" id="about">
     <header>
         <h2 class="section__title section__title--about">Who I'am</h2>
-        <p class="section__subtitle section__subtitle--about">Polyphasetic Software Engineer</p>
+        <p class="section__subtitle section__subtitle--about">Polyfacetic Software Engineer</p>
     </header>
 
     <div class="about-me__body">
